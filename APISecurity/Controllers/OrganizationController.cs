5dd78d4f922f10537955b6872f619610e6ae8a9a
noerromero <noerromero@gmail.com>
2020-02-22 23:02:28 -0500

    Creacion del proyecto

---
+++ APISecurity/Controllers/OrganizationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APISecurity.Controllers
+{
+    [Route("api/organizations")]
+    public class OrganizationController: Controller
+    {
+        [HttpGet()]
+        public JsonResult GetOrganization() {
+            return new JsonResult(new List<object>()
+            {
+                new { OrganizationId = 1, Name = "Organizacion1"},
+                new { OrganizationId = 1, Name = "Organizacion2"}
+            });
+        }
+    }
+}
